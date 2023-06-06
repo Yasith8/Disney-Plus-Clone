c718390b6147be1c91c7b3c390dc5a517681486c
Yasith <yasithnawodya9@gmail.com>
2023-06-06 08:23:39 +0530

    debug the app.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './App.css'
 import Header from './Components/Header';
-import Slider from './Components/slider';
+import Slider from './Components/Slider';
 import ProductionHouse from './Components/ProductionHouse';
 import GenreMovieList from './Components/GenreMovieList';
 import Footer from './Components/Footer';
